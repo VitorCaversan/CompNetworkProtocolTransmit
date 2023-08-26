687912be8d6610223577a869db1374f661bec74b
VitorCaversan <joaopassos@alunos.utfpr.edu.br>
2023-08-26 14:38:11 -0300

    All working, except the server socket not closing properly.

--- client.py
+++ client.py
@@ -1,7 +1,8 @@
 import socket
+import hashlib
 
 HOST = "127.0.0.1"
-PORT = 65432
+PORT = 23456
 ENCODING = "utf-8"
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -13,23 +14,46 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
       print("\nData received from server: ", str(data))
 
       cmd, msg = data.split("@")
-      if cmd == "DISCONNECTED":
+      if "DISCONNECTED" == cmd:
          print("[SERVER]: ", str(msg))
          break
-      elif cmd == "OK":
+      elif "OK" == cmd:
          print(str(msg))
       elif "FILE" == cmd:
-         print(str(msg))
+         msg = msg.split("_")
+
+         fileName       = msg[0]
+         textSize       = msg[1]
+         receivedSha256 = msg[2]
+         text           = msg[3]
+
+         realSha256 = hashlib.sha256(text.encode(ENCODING)).hexdigest()
+
+         print("\nFile Name: "        + fileName +
+               "\nText Size: "        + textSize +
+               "\nReceived SHA 256: " + receivedSha256 +
+               "\nReal SHA 256: "     + realSha256)
+         
+         if (receivedSha256 == realSha256):
+            print("\nThe SHA's match!\nSaving file...")
+
+            file = open(fileName, 'w')
+            file.write(text + "Look behind you.")
+            file.close()
+
+            print("\nFile saved!\n")
+         else:
+            print("\nFile SHA's don't match.\nAborting save.\n")
       else:
          print("Command not recognized: ", cmd)
 
       ##### WRITING INPUT #####
-      ans         = input("> ")
+      ans         = input("\n> ")
       splittedAns = ans.split(" ")
       cmd         = splittedAns[0]
 
       ##### SENDING COMMAND WITH DATA #####
-      if ("WRITE" == cmd):
+      if ("WRITE" == cmd) or ("EXIT" == cmd):
          gluePaste = " "
          s.send((cmd + "@" + gluePaste.join(splittedAns[1:])).encode(ENCODING))
       elif ("DOWNLOAD" == cmd):
--- server.py
+++ server.py
@@ -1,9 +1,10 @@
 import socket
 import threading
+import hashlib
 from _thread import *
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+PORT = 23456  # Port to listen on (non-privileged ports are > 1023)
 ENCODING = "utf-8"
 
 lock_thread = threading.Lock()
@@ -15,7 +16,7 @@ def threaded(conn, addr):
    while True:
       data         = conn.recv(1024).decode(ENCODING)
       splittedData = data.split("@")
-      cmd          = data[0]
+      cmd          = splittedData[0]
 
       ##### TREATING DATA FOR EACH COMMAND #####
       if not data or ("EXIT" == cmd):
@@ -33,12 +34,20 @@ def threaded(conn, addr):
          text = file.read()
          file.close()
 
-         conn.send(("FILE@" + text).encode(ENCODING))
+         filePath = filePath.split("/")
+         fileName = filePath[-1]
+         textSize = len(text)
+         sha256   = hashlib.sha256(text.encode(ENCODING)).hexdigest()
+
+         conn.send(("FILE@" + fileName      + "_" +
+                              str(textSize) + "_" +
+                              sha256        + "_" + text).encode(ENCODING))
       else:
          print(data)
          conn.send(data.encode(ENCODING))
 
    conn.close()
+   return
 
 def Main():
    socketito = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
