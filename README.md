5e984d8ffde3e2ebe90b373e908326e8505aee1c
VitorCaversan <joaopassos@alunos.utfpr.edu.br>
2023-10-24 21:46:51 -0300

    Makes TCP connection work with HTTP request.

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # CompNetworkTCPTransmit
 
-A server Socket TCP connection for receiving files.
+A server Socket for TCP connection that supports multiple connections.
+
+## Functionalities
+It creates a server with a TCP socket that listens to a specific IP and port, given to te user when
+first running the server.py file.
+
+Once the server is running, two options are available:
+ - Connection to the server by running the client.py file:
+    - WRITE _string_: writes the following string in the server terminal;
+    - DOWNLOAD _file name_: downloads a file with the specified _file name_ that is present on the
+      server's folder.
+ - Connection by accessing a web page with the server's ip and port:
+    - It answers to the HTTP GET request with the object of the htmlPage.html file.
--- server.py
+++ server.py
@@ -1,22 +1,36 @@
 import socket
 import threading
 import hashlib
+import os
 from _thread import *
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 23456  # Port to listen on (non-privileged ports are > 1023)
+PORT = 34567  # Port to listen on (non-privileged ports are > 1023)
 ENCODING = "utf-8"
+
+headersDict = {
+'Server': 'Ludwig/1.0',
+'Content-Type': 'text/html'
+}
+
+statusCodesDict = {
+   200: 'OK',
+   404: 'Not Found'
+}
 
 lock_thread = threading.Lock()
 
 def threaded(conn, addr) -> int:
    print(f"[NEW CONNECTION] {addr} connected.")
-   conn.send("OK@Alive and kicking!".encode(ENCODING))
-
+   
    while True:
       data         = conn.recv(1024).decode(ENCODING)
       splittedData = data.split("\r\n")
-      cmd          = splittedData[0]
+
+      if (splittedData[0].find("GET") != -1): # If it is an HTTP request
+         cmd = "GET"
+      else:
+         cmd = splittedData[0]
 
       print("\nData received from client: ", str(data))
 
@@ -26,7 +40,7 @@ def threaded(conn, addr) -> int:
          conn.send("DISCONNECTED@Dead and sleeping".encode(ENCODING))
          break
       elif "WRITE" == cmd:
-         gluePaste = " "
+         gluePaste = " " # Connects the words with a space
          conn.send(("OK@" + gluePaste.join(splittedData[1:])).encode(ENCODING))
       elif "DOWNLOAD" == cmd:
          filePath = splittedData[1]
@@ -43,6 +57,10 @@ def threaded(conn, addr) -> int:
          conn.send(("FILE@" + fileName      + "_" +
                               str(textSize) + "_" +
                               sha256        + "_" + text).encode(ENCODING))
+      elif "GET" == cmd: # HTTP GET
+         response = handleGETRequest(splittedData)
+         print(response.decode("utf-8") + "\n")
+         conn.send(response)
       else:
          print(data)
          conn.send(data.encode(ENCODING))
@@ -83,5 +101,45 @@ def getIP():
    finally:
       s.close()
    return ip
+
+def makeResponseLine(statusCode: int) -> bytes:
+   sttsCodeMeaning = statusCodesDict[statusCode]
+   respLine        = ("HTTP/1.1 " + str(statusCode) + " " + sttsCodeMeaning + "\r\n")
+
+   return respLine.encode("utf-8")
+
+def makeResponseHeaders(extraHeaders:dict = None, contentSize:int = 0) -> bytes:
+   headers = headersDict.copy()
+
+   if (extraHeaders != None):
+      headers.update(extraHeaders)
+
+   headersString = ""
+   for key, value in headers.items():
+      headersString += (key + ": " + value + "\r\n")
+
+   headersString += ("Content-Length: " + str(contentSize) + "\r\n")
+
+   return headersString.encode("utf-8")
+
+def handleGETRequest(splittedData) -> bytes:
+   httpRequest = splittedData[0].split(' ')
+   method      = httpRequest[0]
+   filePath    = httpRequest[1][1:] # Removing the first '/' from the path
+
+   if (os.path.exists(filePath)):
+      file         = open(filePath, "rb")
+      responseBody = file.read()
+      file.close()
+      
+      respLine    = makeResponseLine(200)
+      respHeaders = makeResponseHeaders(contentSize=len(responseBody))
+   else:
+      respLine     = makeResponseLine(404)
+      responseBody = b"<h1>404 Not Found</h1>"
+      respHeaders  = makeResponseHeaders(contentSize=len(responseBody))
+
+   return (respLine + respHeaders + b"\r\n" + responseBody)
+
 
 Main()
